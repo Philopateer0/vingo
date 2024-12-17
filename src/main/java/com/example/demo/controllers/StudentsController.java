9f19c617a7a0d4c848a5f3e30822a2df33853983
Abdallah mohamed <51016060+Feglawy@users.noreply.github.com>
2024-12-17 20:04:52 +0200

    init fixes (#1)
    * ..
    
    * ....
    
    * ..
    
    * ..
    
    ---------
    
    Co-authored-by: islam <islamyasser840@gmail.com>

---
+++ src/main/java/com/example/demo/controllers/StudentsController.java
@@ -0,0 +1,14 @@
+package com.example.demo.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/student")
+@RequiredArgsConstructor
+public class StudentsController {
+
+
+}
