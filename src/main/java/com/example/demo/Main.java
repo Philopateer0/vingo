f59b8098cc99dd64b90b58cbcd1d9430e40297c8
Feglawy <a.m9518520963@outlook.com>
2024-12-17 19:43:39 +0200

    ..

--- src/main/java/com/example/demo/Main.java
+++ src/main/java/com/example/demo/Main.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FcaiLmsApplication {
+public class Main {
 
 	public static void main(String[] args) {
-		SpringApplication.run(FcaiLmsApplication.class, args);
+		SpringApplication.run(Main.class, args);
 	}
 
 }
